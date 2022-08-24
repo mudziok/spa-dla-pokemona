74c4800125c5d5468b5fc717d7abaa5ccde5ce4a
Miłosz Chrzempiec <milosz.chrzempiec@espeo.eu>
2022-08-24 11:01:19 +0200

    Add relations and schemas

---
+++ packages/back/dist/src/api/dog/controllers/dog.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ *  dog controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::dog.dog');
