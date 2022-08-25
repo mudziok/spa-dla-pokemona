1467e96c82ef3988d45cd5e44d473b2733cfbe18
Paulina <paulina.kolasa@espeo.eu>
2022-08-25 08:31:10 +0200

    add registration

--- packages/front/src/components/AddDog/component.tsx
+++ packages/front/src/components/AddDog/component.tsx
@@ -5,7 +5,10 @@ import { AuthContext } from '../../context/authContext';
 export const AddDog = () => {
   const { token } = useContext(AuthContext);
   const [name, setName] = useState('');
+  const [date, setDate] = useState('');
   const [description, setDescription] = useState('');
+  const [registration, setRegistration] = useState('');
+  const [time, setTime] = useState('');
 
   const handleName = (e: ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
@@ -13,6 +16,16 @@ export const AddDog = () => {
   const handleDescription = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setDescription(e.target.value);
   };
+  const handleDate = (e: ChangeEvent<HTMLInputElement>) => {
+    setDate(e.target.value);
+  };
+  const handleTime = (e: ChangeEvent<HTMLInputElement>) => {
+    setTime(e.target.value);
+  };
+
+  useEffect(() => {
+    setRegistration(date + 'T' + time);
+  }, [date, time]);
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -23,6 +36,7 @@ export const AddDog = () => {
           data: {
             name: name,
             problem: description,
+            registration: registration,
           },
         },
         {
@@ -41,7 +55,8 @@ export const AddDog = () => {
         placeholder='description'
         onChange={handleDescription}
       ></textarea>
-      <input type='data'></input>
+      <input type='date' onChange={handleDate}></input>
+      <input type='time' onChange={handleTime}></input>
       <button>Dodaj psa</button>
     </form>
   );
